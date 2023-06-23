20a9897439cc94c4c416f8da12ba15dc8038db49
FlavioACR <flavioabatcarrolar@outlook.com>
2023-06-22 21:58:59 -0600

    Basic structure of router for user object

---
+++ routers/user.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, APIRouter, Request, status
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
+
+
+
+user_router = APIRouter()
+
+template = Jinja2Templates(directory='./templates')
+
+
+
+##--------------------------- ROUTERS & CRUD ---------------------------## 
+
+@user_router.get(path='/signup',
+                 response_class=HTMLResponse,
+                 status_code=status.HTTP_202_ACCEPTED,
+                 summary="Create a User in the App",
+                 tags=["Users"])
+def signup_user(req: Request):
+    return template.TemplateResponse("signup.html", {"request": req})
