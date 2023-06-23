9618ba81f47d3c006d3d78500be571a24cb34855
FlavioACR <flavioabatcarrolar@outlook.com>
2023-06-22 22:17:26 -0600

    Basic structure of module and user services

---
+++ services/user.py
@@ -0,0 +1,13 @@
+
+
+class UserService():
+    '''
+    UserService Class
+    
+    This class contain a logic of the path operations for the object users
+    
+    ''' 
+    def __init__(self, db) -> None:
+        self.db = db
+        
+
